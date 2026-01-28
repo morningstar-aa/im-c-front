24cd00ef4e1c47f554343806598f849eafefb8e6
lx <knowfjfrnewagegj@gmail.com>
2026-01-28 16:18:28 +0800

    fix: add missing Fun...Type typedefs for DLL imports

--- libs/nim_p2p_sdk/include/nim_p2p_sdk_def.h
+++ libs/nim_p2p_sdk/include/nim_p2p_sdk_def.h
@@ -25,4 +25,14 @@ typedef void (*OnChannelCommand)(const RemoteFlagType remote_flag, const char* c
 // Callback for sending commands
 typedef bool (*SendCommandChannel)(const RemoteFlagType remote_flag, const char* command);
 
+// DLL Function types
+typedef OnChannelCommand (*FunInitType)(void*, void*, void*, void*, const char*);
+typedef void (*FunClearType)();
+typedef void (*FunRegTransferFileProcProgType)(const struct TransferFileProcProgRegInfo&);
+typedef TransferFileSessionID (*FunTransferFileType)(const RemoteFlagType, const char*);
+typedef void (*FunCancelTransferFileType)(const TransferFileSessionID&);
+typedef void (*FunReceiveFileType)(const TransferFileSessionID&, const char*);
+typedef void (*FunRejectReceiveFileType)(const TransferFileSessionID&);
+typedef void (*FunCancelReceiveFileType)(const TransferFileSessionID&);
+
 #endif // NIM_P2P_SDK_DEF_H
